cc8ae65ac6a2445fd1aaa52619055422d344bc81
Dylan_Macnab <dylanmacnab@gmail.com>
2017-07-12 21:23:14 -0700

    added play and pause functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,21 +1,31 @@
 // Get The Elements
-
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer')
-const progress = document.querySelector('.progress');
-const progressBar = document.querySelector('.progress__filled');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player-slider');
 
-const toggle = document.querySelector('.toggle');
-const skipButtons = document.querySelectorAll('[data-skip]');
-const ranges = document.querySelectorAll('.player-slider');
+let isPlaying = false;
+
 
 // Build functions
+function playToggle() {
+  if (isPlaying) {
+    video.pause();
+    isPlaying = false;
+  } else {
+    video.play();
+    isPlaying = true;
+  }
+}
 
 // Add Events
+toggle.addEventListener('click', playToggle);
 
 
 /* WHAT I LEARNED:
   1. You can scope the querySelector getter to a parent element!
-  2.
-
+  2. You can use the attribute selector in a querySelectorAll
 */
