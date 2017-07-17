d7d457d95ecf80d04b0ea96e8718ece67b912cbb
Dylan_Macnab <dylanmacnab@gmail.com>
2017-07-16 22:30:33 -0700

    added volume change functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,13 +5,15 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player-slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 let isPlaying = false;
 let cTime = video.currentTime;
 
 
-// Build functions
+// Functions
+
+// Play and Pause
 function playToggle() {
   if (isPlaying) {
     video.pause();
@@ -22,26 +24,33 @@ function playToggle() {
   }
 }
 
-// Progress
+// Progress Bar
 function updateProgressBarWidth() {
-  console.log('I"m working');
   let videoCompletion = 100 * (video.currentTime / video.duration);
-  console.log(videoCompletion);
   progressBar.style.flexBasis = `${videoCompletion}%`;
 }
 
 function updateProgress() {
   updateProgressBarWidth();
   if (isPlaying) {
-    console.log(isPlaying);
     setInterval(updateProgressBarWidth, 1000);
   }
+}
+
+// Volume Controls
+
+function updateVolume() {
+  console.log('event fired');
+  video.volume = ranges[0].value;
+  console.log(volume);
 }
 
 
 // Add Events
 toggle.addEventListener('click', playToggle);
 toggle.addEventListener('click', updateProgress);
+ranges[0].addEventListener('change', updateVolume);
+
 
 /* WHAT I LEARNED:
   1. You can scope the querySelector getter to a parent element!
